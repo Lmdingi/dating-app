23ce1a6ab089d57c9cec57a9f33bbad0f1098bfc
Lmdingi <lmdingi4@gmail.com>
2024-12-30 12:29:29 +0200

    connect API  and get users

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -21,6 +21,8 @@ namespace API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddCors();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -34,6 +36,12 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors(x =>
+            {
+                x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+            });
 
             app.MapControllers();
 
